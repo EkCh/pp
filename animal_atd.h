1671083f9dc7107828290daf7cb70f125d27cb56
EkCh <80422645+EkCh@users.noreply.github.com>
2021-05-27 04:51:35 +0700

    first commit

--- animal_atd.h
+++ animal_atd.h
@@ -1,9 +1,11 @@
+#include <string>
 #pragma once
 namespace animals {
 	enum type { FISH, BIRD };
 	// структура, обобщающая всех имеющихся животных
 	struct animal {
 		type key; // ключ
+		std::string name;
 	};
 
 } // end animals namespac
